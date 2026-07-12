30ecf2b5ff429ebd18d551828bdf00a7244fbf55
Bohbehk <129354842+Bohbehk@users.noreply.github.com>
2026-07-12 16:40:48 -0700

    Added a data base in MongoDB Atlas. Changing the phone number of an existing person now works. There is some error handling with unknown endpoints and wrongly formatted data.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -9,49 +12,30 @@ morgan.token('body', (request, response) => JSON.stringify(request.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }  
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -60,30 +44,70 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'name or number is missing'
         })
-    } else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
+    }
+    Person.find({ name: body.name }).then(result => {
+        if (result.length === 0) { // makes sure names do not repeat in database
+            const person = new Person({
+                name: body.name,
+                number: body.number,
+            })
+
+            person.save().then(savedPerson => {
+                response.json(savedPerson)
+            })
+        } else {
+            response.status(400).json({ error: 'name must be unique' })
+        }
+    })
+
+    
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+    Person.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then(updatedPerson => {
+                response.json(updatedPerson)
+            })
         })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: String(Math.floor(Math.random() * Math.pow(10, 9))),
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    const now = new Date()
-    response.send(`
-        <div>Phonebook has info for ${persons.length} people</div>
-        <div>${now.toString()}<div/>
-    `)
+    Person.find({}).then(persons => {
+        const now = new Date()
+        response.send(`
+            <div>Phonebook has info for ${persons.length} people</div>
+            <div>${now.toString()}<div/>
+        `)
+    })
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+    response.status(400).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint) // handles request of unknown endpoints
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === "CastError") { // handles situations where ids are formatted wrong
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
